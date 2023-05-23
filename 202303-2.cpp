d941b68d2ffbcdeb03bf081c5b4f1504176be077
skymkmk <admin@skymkmk.com>
2023-05-23 22:51:49 +0800

    202303-2 最佳性价比，错误

--- 202303-2.cpp
+++ 202303-2.cpp
@@ -1,10 +1,15 @@
 #include <algorithm>
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-bool compare(pair<int, int> a, pair<int, int> b) {
-    return a.second > b.second;
+bool comparePair(pair<int, float> a, pair<int, float> b) {
+    return a.second < b.second;
+}
+
+bool compare(int a, int b) {
+    return a > b;
 }
 
 int main() {
@@ -12,66 +17,34 @@ int main() {
     int n, m, k;
     cin >> n >> m >> k;
     // t 为开垦耗时，c 为缩短一天所需资源数量。
-    pair<int, int> t[n];
-    int c[n];
+    int t[n], c[n];
     for (int i = 0; i < n; i++) {
-        t[i].first = i;
-        cin >> t[i].second >> c[i];
+        cin >> t[i] >> c[i];
         // 只关注需要缩减的天数
-        t[i].second -= k;
+        t[i] -= k;
+    }
+    pair<int, float> cpt[n];
+    for (int i = 0; i < n; i++) {
+        cpt[i].first = i;
+        cpt[i].second = c[i] * 1.0 / t[i];
+    }
+    while (m > 0) {
+        sort(cpt, cpt + n, comparePair);
+        if(cpt[0].second == numeric_limits<int>::max()) break;
+        int cost = t[cpt[0].first] * c[cpt[0].first];
+        if (cost >= m) {
+            int consume = m / c[cpt[0].first];
+            cost = consume * c[cpt[0].first];
+            m -= cost;
+            t[cpt[0].first] -= consume;
+            break;
+        } else {
+            m -= cost;
+            t[cpt[0].first] = 0;
+            cpt[0].second = numeric_limits<int>::max();
+        }
     }
     sort(t, t + n, compare);
-    while (m > 0) {
-        int consume = 0;
-        if (t[0].second <= 0) break;
-        // 首先考虑非平均情况
-        if (t[0].second > t[1].second) {
-            // 单价
-            int price = c[t[0].first];
-            // 相较下一个田地所能减去的时间
-            consume = t[0].second - t[1].second;
-            int cost = consume * price;
-            if (cost >= m) {
-                t[0].second -= m / price;
-                m = m - m / price;
-                break;
-            } else {
-                t[0].second -= consume;
-                m -= cost;
-            }
-        }
-        else {
-            int same_num = 1, price = c[t[0].first], cost = 0;
-            // 获取所有相同项的总单价
-            for (int i = 0; i < n - 1; i++) {
-                if (t[i].second == t[i + 1].second) {
-                    same_num++;
-                    price += c[t[i + 1].first];
-                }
-            }
-            if (price > m)
-                break;
-            else {
-                if (same_num < n)
-                    consume = t[0].second - t[same_num].second;
-                else
-                    consume = t[0].second;
-                cost = consume * price;
-                if (cost >= m) {
-                    int real_consume = m / price;
-                    for (int i = 0; i < same_num; i++)
-                        t[i].second -= real_consume;
-                    m -= real_consume * price;
-                    break;
-                }
-                else {
-                    for (int i = 0; i < same_num; i++)
-                        t[i].second -= consume;
-                    m -= cost;
-                }
-            }
-        }
-    }
-    cout << t[0].second + k << endl;
+    cout << t[0] + k << endl;
     return 0;
 }
